849ad495410015362bb15382d8af57d0007425f7
PaulineZeziola <paulinez@wanadoo.fr>
2021-08-18 17:06:19 +0200

    add Switch on component Page for display Error 404 Page if page not found

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,6 +1,6 @@
 // imports npm
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // local imports
 import Errors from '../Errors';
 import Home from '../Home';
@@ -9,9 +9,17 @@ import './page.scss';
 
 const Page = () => (
   <Router>
-    <Tour />
-    {/* <Home /> */}
-    {/* <Errors /> */}
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/calendar/day">
+        <Tour />
+      </Route>
+      <Route>
+        <Errors />
+      </Route>
+    </Switch>
   </Router>
 );
 
