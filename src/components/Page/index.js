7618f79d99977292bbe918fe4e1cb0902d6c5116
PaulineZeziola <paulinez@wanadoo.fr>
2021-08-18 13:01:17 +0200

    home page

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 // local imports
 import Errors from '../Errors';
+import Home from '../Home';
 import './page.scss';
 
 const Page = () => (
   <Router>
-    <Errors />
+    <Home />
+    {/* <Errors /> */}
   </Router>
 );
 
