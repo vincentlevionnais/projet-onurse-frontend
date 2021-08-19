eb8fccc4bd919266df39d0053b1155e97b38c040
EliseFoubert <elise.fbt@gmail.com>
2021-08-19 09:44:21 +0200

    integration ok

--- src/components/PatientsList/PatientSmall.js
+++ src/components/PatientsList/PatientSmall.js
@@ -6,7 +6,7 @@ const PatientSmall = () => (
   <article className="patient-small">
 
     <div className="patient-small-content">
-      <h2>Mr Richard Denis</h2>
+      <h2><a href="">Mr Richard Denis</a></h2>
       <p>-</p>
       <p>75 ans</p>
     </div>
