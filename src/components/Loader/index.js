5db46b3d031b55b75c015ba99ced9566c501f736
PaulineZeziola <paulinez@wanadoo.fr>
2021-08-20 11:11:58 +0200

    loader component + scss ok

---
+++ src/components/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import pansement from 'src/assets/images/pansementLoader.png';
+import './loader.scss';
+const Loader = () => (
+  <div className="loader">
+    <div className="loadingio-spinner-spinner-ympf6ujg3pr">
+      <div className="ldio-fr80jed2ehk">
+        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Loader;
+
